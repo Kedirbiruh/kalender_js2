e5a2f96c1a89cb9faf35620f033fcd4af9dfcbb0
Kedirbiruh <kedirbiruh1114@gmail.com>
2025-08-05 02:04:51 +0200

    Refactor HTML structure and enhance calendar functionality in JavaScript and CSS

--- script.js
+++ script.js
@@ -184,3 +184,135 @@ if (istFeiertag) {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const daysContainer = document.querySelector(".days")
+
+
+
+
+
+
+
+
+const days = ['MO', 'DI', 'MI', 'DO', 'FR', 'SA', 'SO'];
+
+// aktuelles Datum siehe oben
+let date = new date();
+// aktueller Monat
+let currentMonth = date.getMonth();
+// aktuelles Jahr
+let currentYeear = date.getFullYear();
+
+// funktion to render days
+function renderCalender() {
+
+    // get prev month, current month, and next month days
+const firstDay = new Date(currentYeear, currentMonth, 1);
+const lastDay = new Date(currentYeear, currentMonth + 1, 0);
+const lastDayIndex = lastDay.getDay();
+const lastDayDate = lastDay.getDate();
+const prevLastDay = new Date(currentYeear, currentMonth, 0);
+const prevLastDayDate = prevLastDay.getDate();
+const nextDays = 7 - lastDayIndex - 1;
+
+
+// update current year abd month in header
+month.innertHTML = `${months[currentMonth]} ${currentYeear}`
+
+// update days html
+let days = "";
+
+// prev days html
+for (let x = firstDay.getDay(); x > 0; x--) {
+
+    days += `<div class="day prev">${prevLastDayDate - x + 1}</div>';
+    }
+
+        // current month days
+
+for (let i = 1; i <= lastDayDate; i++) {
+
+            // check if its today zhen add today class
+        if(i === new Date.getDate() && currentMonth === new Date().getMonth() &&
+             currentYeear().getFullYear()
+        ) 
+        // if date month year matches add today
+        days += `<div class="day today">${i}</div>'
+        
+        }  else {
+
+                    // dont add today
+        days += `<div class= "day">${i}</div>';
+        }
+    } 
+        
+            /next month days 
+for (let j = 1; j <= nextDays; j++){
+       days +=  `<div class= "day next">${j}</div>';    
+        }            
+        daysContainer.innerHTML = days;
+
+    }
+
+        renderCalender();
+
+        nextBtn.addEventListener("click", () => {
+
+            // increase current month by one
+            currentMonth++;
+            if(currentMonth > 11){
+                // if a monthgets greater that 11 make it 0 and increase year by one
+                currentMonth=0;
+                currentYeear++;
+            }
+            
+            // render calender
+            renderCalender();
+        });
+
+        // prev month Btn
+
+        prevBtn.addEventListener("click", () = > {
+            // increase by one
+            currentMonth--;
+            // check if let than 0the nmake it 11 and decrease year
+            if (currentMonth < 0) {
+                currentMonth =11;
+                currentYeear--;
+            }
+            renderCalender();
+            
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
